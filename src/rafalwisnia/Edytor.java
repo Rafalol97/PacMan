2263d115ba2c024f5e8154c252597d6dfcdf3d7c
Kamil Wiśniewski <kamil.wisniewski@student.wat.edu.pl>
2019-06-18 11:37:39 +0200

    zrobilem porzadek z menu

---
+++ src/rafalwisnia/Edytor.java
@@ -0,0 +1,17 @@
+package rafalwisnia;
+
+import javax.swing.*;
+
+public class Edytor extends JPanel {
+
+    public JFrame frame;
+
+    public Edytor() {
+        frame = new JFrame();
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setSize(600, 800);
+        frame.setLocationRelativeTo(null);
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
